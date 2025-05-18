5e57ae44b04cdaf4b6d3660c5e5648d8aea15745
Jonathan Mattingly <jonm@math.duke.edu>
2025-05-17 20:52:30 -0600

    add masic example

--- JuliaExamples/read_atlas.jl
+++ JuliaExamples/read_atlas.jl
@@ -1,15 +1,22 @@
 ## Usage
-# julia read_atlas.jl <atlas name>
+# julia read_atlas.jl <atlas name> <optional: num maps>
 #
 ## Example
-# julia read_atlas.jl ../ExampleAtlas/atlas_nc_multiscale.jsonl.gz
+# julia read_atlas.jl ../ExampleAtlas/atlas_truncated_nc_multiscale.jsonl
+# julia read_atlas.jl ../ExampleAtlas/atlas_nc_multiscale.jsonl.gz 20
 
 
 using Pkg
 push!(LOAD_PATH, "../src/");
 using AtlasIO
+@show ARGS
 
-atlasFileName = ARGS[1]
+atlasFileName = ARGS[1] # Get the file name
+if length(ARGS)>1       # get number of maps set to Inf if none given
+    maxMap=parse(Int64,ARGS[2])
+else
+    maxMap=Inf
+end
 
 println("Running....on file ", atlasFileName)
 
@@ -17,15 +24,14 @@ io=smartOpen(atlasFileName, "r")
 atlas=openAtlas(io);
 ################
 mapCount=0
-maxMap=5
- 
 while !AtlasIO.eof(atlas)
     m = nextMap(atlas) 
     global mapCount+=1
-    if mapCount > maxMap 
+    if (mapCount > maxMap)
         break
     end
-    println("map count:", mapCount, "\t map name:",m.name, " \t number of assignments in districting:",length(m.districting))
+    println("map count:", mapCount, "\t map name:",m.name, 
+        " \t number of assignments in districting:",length(m.districting))
 end
 
 
