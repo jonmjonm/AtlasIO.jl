0ce93480895e78b6e1a18a2b045b7880dc5bc3a1
Jonathan Mattingly <jonm@math.duke.edu>
2026-04-13 14:00:36 -0400

    edits to fix python in main

--- PythonExamples/example_atlas.py
+++ PythonExamples/example_atlas.py
@@ -13,11 +13,7 @@ atlasFileName="atlas_truncated_nc_multiscale.jsonl" #Small number of real maps
 pctDataDir="../Shapefile_JSON/"
 pctDataFileName="pct21_20votes_wMCD.json"
 
-<<<<<<< HEAD
-atlas = Atlas.openAtlas(os.path.join(atlasDir,atlasFileName))    
-=======
 atlas = AtlasIO.openAtlas(os.path.join(atlasDir,atlasFileName)) ##updated to AtlasIO   
->>>>>>> ba9308f67cd3a96f18e6e4e91d1da07e5e719e2e
 pctDataFile = open(os.path.join(pctDataDir,pctDataFileName))
 pctData = json.load(pctDataFile)
 
@@ -25,7 +21,7 @@ print(atlas)
 map=[]
 print("\n")
 while map!=None:
-    map=Atlas.nextMap(atlas)
+    map=AtlasIO.nextMap(atlas)
     if map==None:
         break
     print(map)
